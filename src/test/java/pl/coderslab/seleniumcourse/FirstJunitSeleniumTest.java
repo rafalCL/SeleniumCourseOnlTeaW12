a70f6c426da0062283e613ac5e63757ea94e8ce9
Rafal <rafal.opial@coderslab.pl>
2021-12-05 14:17:22 +0100

    add second test. refactor to use BeforeEach

--- src/test/java/pl/coderslab/seleniumcourse/FirstJunitSeleniumTest.java
+++ src/test/java/pl/coderslab/seleniumcourse/FirstJunitSeleniumTest.java
@@ -1,5 +1,6 @@
 package pl.coderslab.seleniumcourse;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,13 +8,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstJunitSeleniumTest {
+    WebDriver driver;
+
     @Test
     public void shouldSearchInQwant() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.qwant.com/");
         WebElement searchInput = driver.findElement(By.name("q"));
         searchInput.sendKeys("coders lab");
         searchInput.submit();
+    }
+
+    @Test
+    public void shouldSearchInDuckDuckGo() {
+        driver.get("https://duckduckgo.com/");
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys("kawalerowicz faraon");
+        searchInput.submit();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        this.driver = new ChromeDriver();
     }
 }
